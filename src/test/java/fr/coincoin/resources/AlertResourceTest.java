1619a8ac030c4ec4a660e9bcf7f101c9c5000323
Vincent Doba <vincent.doba@cedeela.fr>
2014-12-09 13:32:43 +0100

    add ignored test to be corrected by matthieu

--- src/test/java/fr/coincoin/resources/AlertResourceTest.java
+++ src/test/java/fr/coincoin/resources/AlertResourceTest.java
@@ -1,6 +1,8 @@
 package fr.coincoin.resources;
 
 import fr.coincoin.Main;
+import fr.coincoin.domain.Alert;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -8,7 +10,10 @@ import org.testng.annotations.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,6 +39,12 @@ public class AlertResourceTest {
     public void should_coincoin() {
         String responseMsg = target.path("alerts").request().get(String.class);
         assertThat(responseMsg).isEqualTo("Coincoin!");
+    }
+    
+    @Test(enabled = false)
+    public void should_post_alert() {
+        Response response = target.path("alerts").request().post(Entity.entity(new Alert(), MediaType.APPLICATION_JSON_TYPE));
+        assertThat(response.getEntity()).isNotNull();
     }
 
 }
